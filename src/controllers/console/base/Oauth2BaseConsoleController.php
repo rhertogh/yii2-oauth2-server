3d4fc9716e7fcab18b137c44253ae2e27c975356
rhertogh <rhertogh@users.noreply.github.com>
2023-06-18 17:41:28 +0200

    Enh: Verbose option for base console controller

--- src/controllers/console/base/Oauth2BaseConsoleController.php
+++ src/controllers/console/base/Oauth2BaseConsoleController.php
@@ -4,10 +4,26 @@ namespace rhertogh\Yii2Oauth2Server\controllers\console\base;
 
 use rhertogh\Yii2Oauth2Server\Oauth2Module;
 use yii\console\Controller;
+use yii\helpers\ArrayHelper;
 
 /**
  * @property Oauth2Module $module
  */
 class Oauth2BaseConsoleController extends Controller
 {
+    public $verbose = false;
+
+    public function options($actionID)
+    {
+        return ArrayHelper::merge(parent::options($actionID), [
+            'verbose',
+        ]);
+    }
+
+    public function optionAliases()
+    {
+        return ArrayHelper::merge(parent::optionAliases(), [
+            'v' => 'verbose',
+        ]);
+    }
 }
