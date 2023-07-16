56ca65169db7850b15da0a93df31f82f051a3dce
rhertogh <rhertogh@users.noreply.github.com>
2023-07-16 23:28:52 +0200

    Refactor: extracted client table generation to trait from Oauth2ClistClientsAction

--- src/controllers/console/base/traits/GenerateClientsTableTrait.php
+++ src/controllers/console/base/traits/GenerateClientsTableTrait.php
@@ -2,7 +2,47 @@
 
 namespace rhertogh\Yii2Oauth2Server\controllers\console\base\traits;
 
-trait GenerateClientTableTrait
-{
+use rhertogh\Yii2Oauth2Server\interfaces\models\Oauth2ClientInterface;
+use rhertogh\Yii2Oauth2Server\Oauth2Module;
+use yii\console\widgets\Table;
 
+trait GenerateClientsTableTrait
+{
+    /**
+     * @param Oauth2ClientInterface[] $clients
+     * @return string
+     * @throws \Throwable
+     */
+    public function generateClientsTable($clients)
+    {
+        $rows = [];
+        foreach ($clients as $client) {
+            $redirectUris = $client->getRedirectUri();
+            $rows[] = [
+                'id' => $client->getPrimaryKey(),
+                'identifier' => $client->getIdentifier(),
+                'type' => $client->isConfidential() ? 'Confidential' : 'Public',
+                'redirect_uris' => $redirectUris
+                    ? (
+                        $redirectUris[0]
+                        . (count($redirectUris) > 1
+                            ? ' +' . (count($redirectUris) - 1) . ' more'
+                            : '')
+                    )
+                    : '',
+                'grant_types' => implode(', ', Oauth2Module::getGrantTypeIdentifiers($client->getGrantTypes())),
+            ];
+        }
+
+        return Table::widget([
+            'headers' => [
+                'ID',
+                'Identifier',
+                'Type',
+                'Redirect URIs',
+                'Grant Types',
+            ],
+            'rows' => $rows,
+        ]);
+    }
 }
--- src/controllers/console/client/Oauth2ListClientsAction.php
+++ src/controllers/console/client/Oauth2ListClientsAction.php
@@ -2,17 +2,18 @@
 
 namespace rhertogh\Yii2Oauth2Server\controllers\console\client;
 
+use rhertogh\Yii2Oauth2Server\controllers\console\base\traits\GenerateClientsTableTrait;
 use rhertogh\Yii2Oauth2Server\controllers\console\Oauth2ClientController;
-use rhertogh\Yii2Oauth2Server\Oauth2Module;
 use yii\base\Action;
 use yii\console\ExitCode;
-use yii\console\widgets\Table;
 
 /**
  * @property Oauth2ClientController $controller
  */
 class Oauth2ListClientsAction extends Action
 {
+    use GenerateClientsTableTrait;
+
     public function run()
     {
         $module = $this->controller->module;
@@ -25,35 +26,7 @@ class Oauth2ListClientsAction extends Action
             return ExitCode::OK;
         }
 
-        $rows = [];
-        foreach ($clients as $client) {
-            $redirectUris = $client->getRedirectUri();
-            $rows[] = [
-                'id' => $client->getPrimaryKey(),
-                'identifier' => $client->getIdentifier(),
-                'type' => $client->isConfidential() ? 'Confidential' : 'Public',
-                'redirect_uris' => $redirectUris
-                    ? (
-                        $redirectUris[0]
-                        . (count($redirectUris) > 1
-                            ? ' +' . (count($redirectUris) - 1) . ' more'
-                            : '')
-                    )
-                    : '',
-                'grant_types' => implode(', ', Oauth2Module::getGrantTypeIdentifiers($client->getGrantTypes())),
-            ];
-        }
-
-        $this->controller->stdout(Table::widget([
-            'headers' => [
-                'ID',
-                'Identifier',
-                'Type',
-                'Redirect URIs',
-                'Grant Types',
-            ],
-            'rows' => $rows,
-        ]));
+        $this->controller->stdout($this->generateClientsTable($clients));
 
         return ExitCode::OK;
     }
